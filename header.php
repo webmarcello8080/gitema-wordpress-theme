b6d36857d1627e169660501d10f4fcc93f59c687
webmarcello8080 <webmarcello8080@gmail.com>
2020-06-06 11:58:45 +0100

    remove jquery and bootstrap

--- header.php
+++ header.php
@@ -3,14 +3,6 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="<?= THEME_CSS_URL ?>/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="<?= THEME_JS_URL ?>/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="<?= THEME_JS_URL ?>/bootstrap.min.js"></script>
     <?php wp_head(); ?>
 
 </head>
