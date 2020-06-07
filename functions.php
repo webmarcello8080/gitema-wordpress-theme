6b3ec0588b24e6db6995da4163296658aa0f923f
webmarcello8080 <webmarcello8080@gmail.com>
2020-06-07 13:17:37 +0100

    sidebar and widget area added

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 require get_template_directory() . '/inc/constants.php';
 
 /*
- * include CSS function 
+ * include CSS JS files 
  */
 function teogina_script_unqueue(){
 
@@ -17,7 +17,10 @@ function teogina_script_unqueue(){
 }
 add_action( 'wp_enqueue_scripts', 'teogina_script_unqueue' );
 
-function teogina_setup() {
+/*
+ * set menus and menu names
+ */
+function teogina_menu_setup() {
 	
 	add_theme_support('menus');
 	
@@ -28,7 +31,30 @@ function teogina_setup() {
 
 	register_nav_menus( $locations );
 }
-add_action('init', 'teogina_setup');
+add_action('init', 'teogina_menu_setup');
 
+/*
+ * set widgets and widget positions
+ */
+function teogina_widget_setup() {
+	
+	register_sidebar( 
+		array(
+			'name' => 'Sidebar',
+			'id' => 'sidebar-1',
+			'class' => 'sidebar-custom',
+			'description' => 'This is the Standard Sidebar',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h1 class="widget-title">',
+			'after_title'   => '</h1>',
+		)
+	);
+}
+add_action('widgets_init', 'teogina_widget_setup');
+
+/*
+ * set hooks to activate post-thumbnails and custom-background
+ */
 add_theme_support('custom-background');
 add_theme_support('post-thumbnails');
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     get_header();
     echo '<h1>here</h1>';
+    get_template_part( 'template-parts/sidebar' );
     get_footer();
 ?>
