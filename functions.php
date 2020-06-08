4e624ea234f3f5d6cfa9ebce08c1467230a3305e
Teo Oliver <teo.liz.oli@gmail.com>
2020-06-08 19:59:11 +0100

    added gulp with sass and browser-sync support

--- functions.php
+++ functions.php
@@ -6,3 +6,11 @@ define('THEME_PATH', get_template_directory() );
 define('THEME_CSS_URL', get_template_directory_uri() . '/assets/css' );
 define('THEME_JS_URL', get_template_directory_uri() . '/assets/js' );
 define('THEME_VERSION', wp_get_theme()->get( 'Version' ) );
+
+
+function theme_files(){
+  wp_enqueue_style("theme_main_styles", 'THEME_CSS_URL');
+}
+
+add_action("wp_enqueue_scripts", "theme_files");
+   
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
     get_header();
-    echo '<h1>here</h1>';
+    echo '<h1>hello</h1>';
     get_footer();
 ?>
